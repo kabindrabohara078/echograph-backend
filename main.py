c35458e8a3e1e538353d04d0e1b58b513d2da408
kabindrabohara078 <kabindrabohara1@gmail.com>
2026-05-26 22:32:44 -0700

    add filter result

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ def create_memory(memory: MemoryInput):
         "status": "memory stored"
     }
 
+
+
 @app.post('/search')
 def search_memory(search: searchInput):
     query_embedding = generate_embedding(search.query)
@@ -60,22 +62,82 @@ def search_memory(search: searchInput):
 
     cursor.execute(
         """
-        SELECT id, content, embedding <-> %s::vector AS distance FROM memories ORDER BY distance LIMIT 5;
+        SELECT 
+            id,
+            content, 
+            type,
+            score,
+            access_count,
+            created_at,
+            embedding <-> %s::vector AS distance 
+        FROM memories
+        WHERE state = 'active' 
+        ORDER BY distance 
+        LIMIT 3;
         """,
         (query_embedding, )
     )
 
     results = cursor.fetchall()
 
+    memory_ids = [[row[0], row[6]] for row in results]
+
+
+    
+
+
+    # memory_ids = [x for x in memory_ids if x[1] >= 1]
+
+    print('#############################################################')
+    print(memory_ids)
+    print('#############################################################')
+    print('#############################################################')
+
+
+    memory_ids = [x for x in memory_ids if x[1] <= 1]
+
+    print(memory_ids)
+    print('#############################################################')
+
+
+
+    # memory_ids = memory_ids[-1:1:-1]
+
+    memory_ids = [row[0] for row in memory_ids]
+
+
+    if memory_ids:
+        cursor.execute(
+            """
+            UPDATE memories
+            SET 
+                access_count = access_count + 1,
+                last_accessed = NOW()
+            WHERE id = ANY(%s)
+            """,
+            (memory_ids, )
+        )
+    conn.commit()
+
     memories = []
 
     for row in results:
-        memories.append({
-            "id": row[0],
-            "content": row[1],
-            "distance": row[2]
-        })
+        if row[0] in memory_ids:
+            memories.append({
+                "id": row[0],
+                "content": row[1],
+                "type": row[2],
+                "score": row[3],
+                "access_count": row[4],
+                "created_at": row[5],
+                "distance": row[6]
+            })
 
+    if len(memories) == 0:
         return {
+            "results": "No memory available with high confidence!"
+        }
+
+    return {
             "results": memories
         }
